61869b5a6221003a0d3a682cb2718a7b17d7fda4
amir <mirjalili16@gmail.com>
2024-02-14 11:56:59 +0330

    fix :validate api candidate

--- app/api/candidate-survey/route.ts
+++ app/api/candidate-survey/route.ts
@@ -1,19 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import { SurveysGet } from "@/app/api/services/surveys.get";
+import { surveyValidator } from "@/app/api/validator/survey.validator";
 
 export async function GET(req: NextRequest) {
-  // const body = req;
-  // const { error } = surveyValidator.candidate-survey.validate(body);
-  // if (error) {
-  //   const { details } = error;
-  //   const message = details.map((i: any) => i.message).join(",");
-  //   return NextResponse.json({ error: message }, { status: 412 });
-  // }
+  const params = req.nextUrl.searchParams;
+  const { error, value } = surveyValidator.candidate.validate({
+    gender: params.get("gender"),
+    age: params.get("age"),
+  });
+  if (error) {
+    const { details } = error;
+    const message = details.map((i: any) => i.message).join(",");
+    return NextResponse.json({ error: message }, { status: 412 });
+  }
   const surveysGet = new SurveysGet();
   const response = await surveysGet.candidateSurvey(
-    req.nextUrl.searchParams.get("gender")!,
-    Number(req.nextUrl.searchParams.get("age"))!,
-    Number(req.nextUrl.searchParams.get("skip"))!
+    value.gender,
+    Number(value.age),
+    value.skip
   );
   return NextResponse.json(response);
 }
